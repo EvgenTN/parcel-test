c9e0a368b7ddb87f57a9fd5768bd7c70b105e675
evgentn <evgentitovn@gmail.com>
2019-11-04 16:58:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <Card/>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card'
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      bundler: 'parcel'
+    }
+  }
+}
+</script>
